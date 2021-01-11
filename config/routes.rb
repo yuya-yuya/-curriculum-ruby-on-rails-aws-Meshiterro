1ab0a04bee8adbcb754f0c64d267c508327b2e66
yuya-yuya <yytyfp5115@icloud.com>
2021-01-11 07:28:18 +0000

    additional commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 end
